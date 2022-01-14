a572fdca14a6b806b0f232ad534191b10932b1b2
andreeapetrut <91960428+andreeapetrut@users.noreply.github.com>
2022-01-14 15:52:40 +0200

    Add files via upload

---
+++ Pb16/Pb16/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ex16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Numar foi: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Introduceti numarul de randuri / foaie: ");
+            var r = Convert.ToInt32(Console.ReadLine());
+
+            long total = n * r;
+            Console.WriteLine();
+            Console.WriteLine($"Cartea cu {n} pagini si {r} randuri/pagina are {total} randuri in total.");
+        }
+    }
+}
