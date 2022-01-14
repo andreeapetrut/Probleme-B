d122b010e75347aab5f37091d4e85c35723e0358
andreeapetrut <91960428+andreeapetrut@users.noreply.github.com>
2022-01-14 15:51:45 +0200

    Add files via upload

---
+++ Pb13/Pb13/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Ex13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Celsius | Fahrenheit");
+            for (var i = 0; i <= 100; i++)
+            {
+                Console.WriteLine("{0} | {1:f1}", i, i * (9.0 / 5.0) + 32.0);
+            }
+        }
+    }
+}
