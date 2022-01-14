6561b0d73f898242893c9eb7cb0174c1d942b2b3
andreeapetrut <91960428+andreeapetrut@users.noreply.github.com>
2022-01-14 15:47:19 +0200

    Add files via upload

---
+++ Pb1/1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Sa se scrie un program care converteste temperatura din grade celsius in grade fahrenheit si invers utilizand instructiunea switch
+
+            int C, F, opt;
+
+            Console.WriteLine("Meniu");
+            Console.WriteLine("1.Celsius -> Fahrenheit");
+            Console.WriteLine("2.Fahrenheit -> Celsius");
+            opt = Convert.ToInt32(Console.ReadLine());
+
+            switch(opt)
+            {
+                case 1:
+                    C = 0; F = 0;
+                    Console.WriteLine("Introdu gradele Celsius: ");
+                    C = Convert.ToInt32(Console.ReadLine());
+                    F = (C * 18/10 +32);
+                    Console.WriteLine("Gradele Celsius in grade Fahrenheit sunt: "); Console.Write(F);
+                    break;
+                case 2:
+                    C = 0; F = 0;
+                    Console.WriteLine("Introdu gradele Fahrenheit: ");
+                    F = Convert.ToInt32(Console.ReadLine());
+                    C = (F - 32) * 5/9;
+                    Console.WriteLine("Gradele Fahrenheit in grade Celsius sunt: "); Console.Write(C);
+                    break;
+            }
+        }
+    }
+}
