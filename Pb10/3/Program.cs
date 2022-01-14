d609e73c797937d3330ee2d34d6c2fde6328abcf
andreeapetrut <91960428+andreeapetrut@users.noreply.github.com>
2022-01-14 15:50:28 +0200

    Add files via upload

---
+++ Pb10/3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n,i=1;
+            bool prim = true;
+
+            Console.WriteLine("n=");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            while(n!=0)
+            {
+                prim = true;
+                if (i < 2)
+                { 
+                    prim = false;
+                }
+                for (int d = 2; d * d <= n; d++)
+                    if (i % d == 0)
+                    {
+                        prim = false;
+                    }
+                if (prim == true)
+                {
+                    Console.WriteLine(i);
+                    n--;
+                }
+                i++;
+            }
+        }
+    }
+}
