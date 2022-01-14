30dec59c9332b76cc635b132a044f1aeed214c25
andreeapetrut <91960428+andreeapetrut@users.noreply.github.com>
2022-01-14 15:51:06 +0200

    Add files via upload

---
+++ Pb11/Pb11/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a, b, r;
+
+            Console.WriteLine("a= ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("b= ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            r = a % b;
+
+            while (r != 0)
+            {
+                a = b;
+                b = r;
+                r = a % b;
+            }
+
+            if (b == 1)
+            {
+                Console.WriteLine("DA");
+            }
+            else
+            {
+                Console.WriteLine("NU");
+            }
+        }
+    }
+}
